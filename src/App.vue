c7d482158b4df8da9f6940c7eca76338fb5ca1e6
DasERS <nameisdasers@gmail.com>
2024-11-27 23:38:34 -0500

    no message

---
+++ src/App.vue
@@ -0,0 +1,75 @@
+<template>
+  <div id="app">
+    <header>
+      <h1>Gestión de Equipos</h1>
+    </header>
+    <main>
+      <component :is="currentComponent" />
+      <nav>
+        <button @click="prevPage" :disabled="currentIndex === 0">Anterior</button>
+        <button @click="nextPage" :disabled="currentIndex === components.length - 1">Siguiente</button>
+      </nav>
+    </main>
+  </div>
+</template>
+
+<script>
+import DatosEquipo from './components/datosEquipo.vue';
+import ConfigEquipo from './components/configEquipo.vue';
+import ConfigRed from './components/configRed.vue';
+import ControlCambios from './components/controlCambios.vue';
+import FormUbicacion from './components/formMantenimiento.vue';
+import formMantenimiento from './components/formUbicacion.vue';
+
+
+export default {
+  data() {
+    return {
+      currentIndex: 0,
+      components: [
+        'DatosEquipo',
+        'ConfigEquipo',
+        'ConfigRed',
+        'ControlCambios',
+        'Ubi',
+        'Mantenimiento',
+      ],
+    };
+  },
+  computed: {
+    currentComponent() {
+      return this.components[this.currentIndex];
+    },
+  },
+  components: {
+    DatosEquipo,
+    ConfigEquipo,
+    ConfigRed,
+    ControlCambios,
+    FormUbicacion,
+    formMantenimiento,
+  },
+  methods: {
+    nextPage() {
+      if (this.currentIndex < this.components.length - 1) {
+        this.currentIndex++;
+      }
+    },
+    prevPage() {
+      if (this.currentIndex > 0) {
+        this.currentIndex--;
+      }
+    },
+  },
+};
+</script>
+
+<style>
+/* Añade estilo básico para la navegación */
+nav {
+  margin-top: 20px;
+}
+button {
+  margin: 5px;
+}
+</style>
